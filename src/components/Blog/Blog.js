31ede3c3d3c170bee5557fc891f01beb80abf0b8
abusayeed4bd <abusayeed4bd@gmail.com>
2022-04-05 15:36:26 +0600

    blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,7 +4,23 @@ import './Blog.css'
 const Blog = () => {
     return (
         <div>
-            <h2>this is blog page</h2>
+            <div className="question">
+                <h1 className='orange-color'>What is context api?</h1>
+                <hr />
+                <p>Context api is a way to transfer data to a component tree without using props.Some time wee need to pass data from a component to another component that can be much lower lavel then the parent component. we have to pass data throw every component betweenn these 2 components by props but if we use context api we can get data from any components in the component tree wihtout drilling props.</p>
+            </div>
+            <div className="question">
+                <h1 className='orange-color'>Whai is block and inline-block element?</h1>
+                <hr />
+                <p>
+                    <b>Block:</b>
+                    Block element is some tag in html that took full line and it can be aligned in the line anywhere. block element can took height and weight.
+                </p>
+                <p>
+                    <b>Inline-Block:</b>
+                    Inline-block element act like both inline and block element. it do not take full line space but inline-block element can get hight and witght.
+                </p>
+            </div>
         </div>
     );
 };
